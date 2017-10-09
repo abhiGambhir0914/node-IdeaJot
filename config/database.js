646a06b8ed313fe30853fcb0eedad6e90f93c7c2
System Administrator <root@Neelams-MacBook-Pro.local>
2017-10-09 22:23:41 +0530

    Changed user of database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://user:illberichoneday@ds113775.mlab.com:13775/node-ideajot'}
+  module.exports = {mongoURI: 'mongodb://abhi:illberichoneday@ds113775.mlab.com:13775/node-ideajot'}
 }else {
   module.exports = {mongoURI: 'mongodb://localhost/vididea-dev'}
 }
